073d1beec1b5327d701a482a2b6bbe6b123a0d4b
thearchivalone <plow-yodel-come@duck.com>
2026-03-22 12:29:32 -0500

    Init: C Std Containers decomp

---
+++ decomp/Std-Containers.c
@@ -0,0 +1,7 @@
+// What would normally be Lists, Tables and other Std Containers, these are the engine's custom equivalents
+void set_tbl_data(undefined4 data)
+{
+  tbl_data = data;
+  return;
+}
+
